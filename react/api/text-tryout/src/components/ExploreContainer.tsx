95706f468a34780961c2248170aeb8e2717d09fe
louiscklaw <louiscklaw@users.noreply.github.com>
2023-01-19 18:51:21 +0800

    update,

--- react/api/text-tryout/src/components/ExploreContainer.tsx
+++ react/api/text-tryout/src/components/ExploreContainer.tsx
@@ -1,16 +1,40 @@
 import './ExploreContainer.css';
 
+import React from 'react';
+import { IonText, IonIcon } from '@ionic/react';
+import { warning } from 'ionicons/icons';
+
 interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
+      <IonText color="primary">
+        <h1>H1: The quick brown fox jumps over the lazy dog</h1>
+      </IonText>
+
+      <IonText color="secondary">
+        <h2>H2: The quick brown fox jumps over the lazy dog</h2>
+      </IonText>
+
+      <IonText color="tertiary">
+        <h3>H3: The quick brown fox jumps over the lazy dog</h3>
+      </IonText>
+
       <p>
-        Start with Ionic{' '}
-        <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">
-          UI Components
-        </a>
+        <IonText color="warning">
+          <IonIcon icon={warning}></IonIcon>
+        </IonText>
+        I saw a werewolf with a Chinese menu in his hand. Walking through the{' '}
+        <IonText color="success">
+          <sub>streets</sub>
+        </IonText>{' '}
+        of Soho in the rain. He{' '}
+        <IonText color="medium">
+          <i>was</i>
+        </IonText>{' '}
+        looking for a place called Lee Ho Fook's. Gonna get a{' '}
+        <IonText color="danger">big dish of beef chow mein.</IonText>
       </p>
     </div>
   );
