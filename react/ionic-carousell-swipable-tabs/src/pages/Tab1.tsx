daf860ae766a02ce26fa71c1313fa5019e52709e
louiscklaw <louiscklaw@users.noreply.github.com>
2023-02-04 02:43:41 +0800

    update ionic-swipeable-tabs,

--- react/ionic-carousell-swipable-tabs/src/pages/Tab1.tsx
+++ react/ionic-carousell-swipable-tabs/src/pages/Tab1.tsx
@@ -1,16 +1,26 @@
-import { IonCard, IonContent, IonHeader, IonPage, IonSegment, IonSegmentButton, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonCard,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonSegment,
+  IonSegmentButton,
+  IonTitle,
+  IonToolbar,
+  IonAvatar,
+} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import { SuperTabsContainer, SuperTab, SuperTabs, SuperTabsToolbar, SuperTabButton } from '@ionic-super-tabs/react';
 import React, { useRef, useState } from 'react';
-import { IonChip, IonSlide,IonSlides,IonItem, IonItemDivider, IonItemGroup, IonLabel, IonList } from '@ionic/react';
+import { IonChip, IonSlide, IonSlides, IonItem, IonItemDivider, IonItemGroup, IonLabel, IonList } from '@ionic/react';
 
 // core/dist/types/super-tabs-toolbar/super-tabs-toolbar.component.d.ts
 const Tab1: React.FC = () => {
   const [segment, setSegment] = useState<string>('default1');
   const [value, setValue] = useState('0');
 
-  const list =['default1', 'default2','default3','default4','default5','default6','default7','default8']
+  const list = ['default1', 'default2', 'default3', 'default4', 'default5', 'default6', 'default7', 'default8'];
 
   const handleSlideChange = async (event: any) => {
     let index: number = 0;
@@ -19,7 +29,7 @@ const Tab1: React.FC = () => {
     });
     setSegment(list[index]);
     setValue('' + index);
-  }
+  };
   const slideOpts = {
     initialSlide: 0,
     speed: 400,
@@ -47,9 +57,7 @@ const Tab1: React.FC = () => {
 
         <IonList>
           <IonItemGroup>
-            <IonItemDivider sticky>
-              <IonLabel>Section A</IonLabel>
-            </IonItemDivider>
+            <IonItemDivider sticky></IonItemDivider>
 
             <IonItem>
               <IonLabel>A1</IonLabel>
@@ -80,89 +88,311 @@ const Tab1: React.FC = () => {
 
           <IonItemGroup>
             <IonItemDivider sticky>
-              <IonLabel>Section B</IonLabel>
+              <IonSegment mode={'md'} scrollable={true} value={segment} style={{paddingBottom:'10px'}}>
+                <IonSegmentButton value="default1">
+                  <div
+                    style={{
+                      width: '80px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      rowGap: '5px',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: segment == 'default1' ? 'rgba(220,220,220,0.9)' : 'white',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: '50px',
+                        height: '50px',
+                        borderRadius: '10px',
+                        backgroundImage: 'url("https://images.unsplash.com/photo-1568901346375-23c9450c58cd")',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                      }}
+                    ></div>
+                    <div style={{ fontSize: '0.8rem', color: 'rgba(12,12,12,0.8)' }}>勤常勉旋</div>
+                  </div>
+                </IonSegmentButton>
+                <IonSegmentButton value="default2">
+                  <div
+                    style={{
+                      width: '80px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      rowGap: '5px',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: segment == 'default2' ? 'rgba(220,220,220,0.9)' : 'white',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: '50px',
+                        height: '50px',
+                        borderRadius: '10px',
+                        backgroundImage: 'url("https://images.unsplash.com/photo-1568901346375-23c9450c58cd")',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                      }}
+                    ></div>
+                    <div style={{ fontSize: '0.8rem', color: 'rgba(12,12,12,0.8)' }}>勤常勉旋</div>
+                  </div>
+                </IonSegmentButton>
+
+                <IonSegmentButton value="default3">
+                  <div
+                    style={{
+                      width: '80px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      rowGap: '5px',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: segment == 'default3' ? 'rgba(220,220,220,0.9)' : 'white',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: '50px',
+                        height: '50px',
+                        borderRadius: '10px',
+                        backgroundImage: 'url("https://images.unsplash.com/photo-1568901346375-23c9450c58cd")',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                      }}
+                    ></div>
+                    <div style={{ fontSize: '0.8rem', color: 'rgba(12,12,12,0.8)' }}>勤常勉旋</div>
+                  </div>
+                </IonSegmentButton>
+
+                <IonSegmentButton value="default4">
+                  <div
+                    style={{
+                      width: '80px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      rowGap: '5px',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: segment == 'default4' ? 'rgba(220,220,220,0.9)' : 'white',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: '50px',
+                        height: '50px',
+                        borderRadius: '10px',
+                        backgroundImage: 'url("https://images.unsplash.com/photo-1568901346375-23c9450c58cd")',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                      }}
+                    ></div>
+                    <div style={{ fontSize: '0.8rem', color: 'rgba(12,12,12,0.8)' }}>勤常勉旋</div>
+                  </div>
+                </IonSegmentButton>
+                <IonSegmentButton value="default5">
+                  <div
+                    style={{
+                      width: '80px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      rowGap: '5px',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: segment == 'default5' ? 'rgba(220,220,220,0.9)' : 'white',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: '50px',
+                        height: '50px',
+                        borderRadius: '10px',
+                        backgroundImage: 'url("https://images.unsplash.com/photo-1568901346375-23c9450c58cd")',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                      }}
+                    ></div>
+                    <div style={{ fontSize: '0.8rem', color: 'rgba(12,12,12,0.8)' }}>勤常勉旋</div>
+                  </div>
+                </IonSegmentButton>
+                <IonSegmentButton value="default6">
+                  <div
+                    style={{
+                      width: '80px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      rowGap: '5px',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: segment == 'default6' ? 'rgba(220,220,220,0.9)' : 'white',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: '50px',
+                        height: '50px',
+                        borderRadius: '10px',
+                        backgroundImage: 'url("https://images.unsplash.com/photo-1568901346375-23c9450c58cd")',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                      }}
+                    ></div>
+                    <div style={{ fontSize: '0.8rem', color: 'rgba(12,12,12,0.8)' }}>勤常勉旋</div>
+                  </div>
+                </IonSegmentButton>
+
+                <IonSegmentButton value="default7">
+                  <div
+                    style={{
+                      width: '80px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      rowGap: '5px',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: segment == 'default7' ? 'rgba(220,220,220,0.9)' : 'white',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: '50px',
+                        height: '50px',
+                        borderRadius: '10px',
+                        backgroundImage: 'url("https://images.unsplash.com/photo-1568901346375-23c9450c58cd")',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                      }}
+                    ></div>
+                    <div style={{ fontSize: '0.8rem', color: 'rgba(12,12,12,0.8)' }}>勤常勉旋</div>
+                  </div>
+                </IonSegmentButton>
+
+                <IonSegmentButton value="default8">
+                  <div
+                    style={{
+                      width: '80px',
+                      display: 'flex',
+                      flexDirection: 'column',
+                      rowGap: '5px',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: segment == 'default8' ? 'rgba(220,220,220,0.9)' : 'white',
+                      borderRadius: '10px',
+                    }}
+                  >
+                    <div
+                      style={{
+                        width: '50px',
+                        height: '50px',
+                        borderRadius: '10px',
+                        backgroundImage: 'url("https://images.unsplash.com/photo-1568901346375-23c9450c58cd")',
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat',
+                      }}
+                    ></div>
+                    <div style={{ fontSize: '0.8rem', color: 'rgba(12,12,12,0.8)' }}>勤常勉旋</div>
+                  </div>
+                </IonSegmentButton>
+              </IonSegment>
             </IonItemDivider>
 
-            <IonSegment mode={'md'} scrollable={true} value={segment} >
-              <IonSegmentButton value="default1">
-                <IonChip>Default1</IonChip>
-              </IonSegmentButton>
-              <IonSegmentButton value="default2">
-                <IonLabel>Default2</IonLabel>
-              </IonSegmentButton>
+            <IonItem>
+              <IonSlides pager={true} options={slideOpts} onIonSlideDidChange={e => handleSlideChange(e)} ref={slider}>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>1111</IonCard>
+                </IonSlide>
 
-              <IonSegmentButton value="default3">
-                <IonLabel>Default3</IonLabel>
-              </IonSegmentButton>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>2222</IonCard>
+                </IonSlide>
 
-              <IonSegmentButton value="default4">
-                <IonLabel>Default4</IonLabel>
-              </IonSegmentButton>
-              <IonSegmentButton value="default5">
-                <IonLabel>Default</IonLabel>
-              </IonSegmentButton>
-              <IonSegmentButton value="default6">
-                <IonLabel>Default2</IonLabel>
-              </IonSegmentButton>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>3333</IonCard>
+                </IonSlide>
 
-              <IonSegmentButton value="default7">
-                <IonLabel>Default3</IonLabel>
-              </IonSegmentButton>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>4444</IonCard>
+                </IonSlide>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>5</IonCard>
+                </IonSlide>
 
-              <IonSegmentButton value="default8">
-                <IonLabel>Default4</IonLabel>
-              </IonSegmentButton>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>6</IonCard>
+                </IonSlide>
 
-            </IonSegment>
-              <IonItem>
-                <IonSlides
-                  pager={true}
-                  options={slideOpts}
-                  onIonSlideDidChange={e => handleSlideChange(e)}
-                  ref={slider}
-                >
-                  <IonSlide>
-                    <IonCard  >1111
-                      </IonCard>
-                  </IonSlide>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>7</IonCard>
+                </IonSlide>
 
-                  <IonSlide>
-                    <IonCard  >2222
-                      </IonCard>
-                  </IonSlide>
-
-                  <IonSlide>
-                    <IonCard  >3333
-                      </IonCard>
-                  </IonSlide>
-
-                  <IonSlide>
-                    <IonCard  >4444
-                      </IonCard>
-                  </IonSlide>
-                  <IonSlide>
-                    <IonCard  >5
-                      </IonCard>
-                  </IonSlide>
-
-                  <IonSlide>
-                    <IonCard  >6
-                      </IonCard>
-                  </IonSlide>
-
-                  <IonSlide>
-                    <IonCard  >7
-                      </IonCard>
-                  </IonSlide>
-
-                  <IonSlide>
-                    <IonCard  >8
-                      </IonCard>
-                  </IonSlide>
-                </IonSlides>
-              </IonItem>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>8</IonCard>
+                </IonSlide>
+              </IonSlides>
+            </IonItem>
           </IonItemGroup>
 
+          <IonItemGroup>
+            <IonItemDivider sticky>
+              <div>
+                <div style={{ margin: '10px' }}>
+                  <IonLabel>Section B</IonLabel>
+                </div>
+                <div style={{ margin: '10px' }}></div>
+              </div>
+            </IonItemDivider>
+
+            <IonItem>
+              <IonSlides pager={true} options={slideOpts} onIonSlideDidChange={e => handleSlideChange(e)} ref={slider}>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>1111</IonCard>
+                </IonSlide>
+
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>2222</IonCard>
+                </IonSlide>
+
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>3333</IonCard>
+                </IonSlide>
+
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>4444</IonCard>
+                </IonSlide>
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>5</IonCard>
+                </IonSlide>
+
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>6</IonCard>
+                </IonSlide>
+
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>7</IonCard>
+                </IonSlide>
+
+                <IonSlide style={{ height: '100vh' }}>
+                  <IonCard>8</IonCard>
+                </IonSlide>
+              </IonSlides>
+            </IonItem>
+          </IonItemGroup>
 
           <IonItemGroup>
             <IonItemDivider sticky>
